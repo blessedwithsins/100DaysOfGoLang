2cfd02d48cb70f39d39ea868c54995e63e8fed39
professorwardaddy <107780440+pigaur@users.noreply.github.com>
2025-03-10 09:19:28 +0530

    Add files via upload

---
+++ Day1/main.go
@@ -0,0 +1,23 @@
+// Main is a special package which contains the main function which is the entry point of the executable programs.
+
+package main
+
+// Importing the fmt package which is used for formatting I/O.
+
+import "fmt"
+
+// fmt package is used to print the output to the console.
+
+// . is member access operator in Go.
+
+// Println is a function in fmt package which is used to print the output to the console. It prints and adds a new line at the end.
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+/*
+Multi
+Line
+Comment
+*/
